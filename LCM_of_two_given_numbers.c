c6f461b3023882ae1f272171f95244f60199a2a4
koradasandhyadevi <119841076+koradasandhyadevi@users.noreply.github.com>
2022-12-31 18:30:40 +0530

    #include<stdio.h> int main() {     int a,b,gcd=0,lcm,i;     scanf("%d%d",&a,&b);     for(i=1;i<=a&&i<=b;i++)     {         if(a%i==0&&b%i==0)         {             gcd=i;         }     } lcm=a*b/gcd; printf("%d",lcm); }

--- LCM_of_two_given_numbers.c
+++ LCM_of_two_given_numbers.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,gcd,lcm;
+    int a,b,gcd=0,lcm,i;
     scanf("%d%d",&a,&b);
-    for(i=1;i<=a && i<=b;i++)
+    for(i=1;i<=a&&i<=b;i++)
     {
-        if(a%i==0 && b%i==0)
-        gcd=i;
+        if(a%i==0&&b%i==0)
+        {
+            gcd=i;
+        }
     }
-    lcm=a*b/gcd;
-    printf("%d",lcm);
+lcm=a*b/gcd;
+printf("%d",lcm);
 }
