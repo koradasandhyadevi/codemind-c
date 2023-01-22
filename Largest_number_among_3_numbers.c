53ca62f51ff0ad23d3999f182e9592c995ecc863
koradasandhyadevi <119841076+koradasandhyadevi@users.noreply.github.com>
2023-01-22 21:44:58 +0530

    #include<stdio.h> int main() {     int a,b,c,big;     scanf("%d%d%d",&a,&b,&c);     big=a>b?(a>c?a:c):(b>c?b:c);     printf("%d",big); }

--- Largest_number_among_3_numbers.c
+++ Largest_number_among_3_numbers.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,largest;
+    int a,b,c,big;
     scanf("%d%d%d",&a,&b,&c);
-    largest=a>b?(a>c?a:c):(b>c?b:c);
-    printf("%d",largest);
+    big=a>b?(a>c?a:c):(b>c?b:c);
+    printf("%d",big);
 }
