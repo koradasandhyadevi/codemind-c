f466a1ba35afebea95fb44e1e36825b10a9bac0f
koradasandhyadevi <119841076+koradasandhyadevi@users.noreply.github.com>
2023-01-23 19:30:00 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a+c>b&&a+b>c&&b+c>a)     {         printf("Valid triangle");     }     else     {         printf("Invalid triangle");     } }

--- Triangle_valid_or_not.c
+++ Triangle_valid_or_not.c
@@ -3,13 +3,12 @@ int main()
 {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    if(a+b>c&&a+c>b&&b+c>a)
-{
-    printf("Valid triangle");
-}
-else
-{
-    printf("Invalid triangle");
-}
-    
+    if(a+c>b&&a+b>c&&b+c>a)
+    {
+        printf("Valid triangle");
+    }
+    else
+    {
+        printf("Invalid triangle");
+    }
 }
